fec1f0e7f85885c3ccf1422f866fa23f865f524d
slagesse <shanelagesse@gmail.com>
2026-03-20 16:52:21 -0500

    Https

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(301, 'https://' + req.hostname + req.url);
+  }
+  next();
+});
+
 // Serve static files (CSS, JS, images, etc.)
 app.use(express.static(__dirname));
 
